6224b892a360a14054afa601b41770b436458e14
Si Chen <victor.chensi@gmail.com>
2016-02-25 17:28:50 -0800

    oop RegressionRunner

--- run_spectra_wireless_testcases.py
+++ run_spectra_wireless_testcases.py
@@ -10,35 +10,73 @@ def grep(path, regex):
                 if regObj.search(line):
                     return True
 
+class Workspace (object):
+    def __init__(self, directory = '', branch = 'macallan_dev'):
+        self.ws_dir = directory
+        self.branch = branch
+        self.binos_root = "%s/binos" % self.ws_dir if self.ws_dir != '' else ''
+
+    def pull (self):
+        """Pull work space"""
+        if self.branch == '': 
+            raise ValueError("Branch is not set")
+        if self.ws_dir == '':
+           raise ValueError("workspace dir is not set")
+
+        try:
+            if not os.path.exists(self.ws_dir):
+                os.makedirs(self.ws_dir)
+            os.chdir(self.ws_dir)
+            cmd = "acme nw -project %s -sb xe" % (self.branch)
+            print("Executing (%s)" % (cmd))
+            subprocess.check_call(
+                cmd, stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as e:
+            if e.returncode != 255:
+                print("###Error in pulling workspace", e.returncode)
+                raise
+            
+    def build (self, target = 'x86_64_binos_root'):
+        if grep('%s/BUILD_LOGS/' % self.binos_root,
+                'SUCC.*x86_64_binos_root'):
+            print('x86_64_binos_root already built')
+            return
+
+        cmd = "mcp_ios_precommit -- -j16 build_x86_64_binos_root"
+        print("Executing (%s)" % (cmd))
+        try:
+            os.chdir("%s/ios/sys" % (self.ws_dir))
+            subprocess.check_call(
+                cmd, stderr=subprocess.STDOUT, shell=True)
+        except subprocess.CalledProcessError as e:
+            print("###Error in building binos linkfarm")
+            # Send email for build failure
+            send_email_binos_root(email, self.binos_root, bugs_file)
+        except Exception as ex:
+            template = "An exception of type {0} occured. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print message
+
+    def __repr__ (self) :
+        return "<Workspace of branch %s at BINOS_ROOT=%s>" % (self.branch, self.binos_root)
+
+class RegressionRunner(object):
+    ws = None
+    def prepare(self):
+        if self.ws is None:
+            raise Exception('workspace not set')
+
+        print("Workspace: {}".format(self.ws))
+        self.ws.pull()
+        self.ws.build()
+
 
 if __name__ == "__main__":
+    STORAGE = '/scratch/siche'
     WS_NAME = "mac_" + datetime.datetime.now().strftime('%Y-%m-%d')
-    try:
-        retcode = subprocess.call("iws -l latest -t " + WS_NAME +
-                   " -xe macallan_dev -d /scratch/siche/", shell=True)
-        print "iws returned %s" % (retcode)
-
-        WS_NAME = "iws_" + WS_NAME
-        WS_DIR = "/scratch/siche/"+WS_NAME
-        os.chdir(WS_DIR)
-        BINOS_ROOT=os.getcwd()+"/binos"
-
-        build_env = os.environ.copy()
-        build_env["PATH"] = build_env["PATH"] + ":/auto/binos-tools/bin/"
-        build_env["BINOS_ROOT"] = BINOS_ROOT
-        
-        if not grep(BINOS_ROOT+"/BUILD_LOGS/", 'SUCCESS.*build_x86_64_binos_root'):
-            os.chdir("ios/sys")
-            retcode = subprocess.call("mcp_ios_precommit build_x86_64_binos_root",
-                                      env=build_env,
-                                      shell=True)
-            print "mcp_ios_precommit returned %s" % retcode
-
-        build_env["INSTALL_DIR_PATH"] = BINOS_ROOT
-        subprocess.call(["python", BINOS_ROOT + "/platforms/ngwc/doppler_sdk/tools/scripts/spectra_build.py",
-                        "-p", "-a DopplerD"]);
-
-    except subprocess.CalledProcessError as e:
-        print "CalledProcessError", e
-    except OSError as e:
-        print >>sys.stderr, "Execution failed:", e
+    WS_NAME = 'gogogo'
+    r = RegressionRunner();
+    w = Workspace(directory = "%s/%s" % (STORAGE, WS_NAME))
+    r.ws = w;
+    r.prepare()
+    
