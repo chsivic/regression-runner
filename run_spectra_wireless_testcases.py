50d3d511e8d20bb3b03c0f38887d4644b31d39ce
Si Chen <victor.chensi@gmail.com>
2016-02-26 14:58:22 -0800

    save changes to run_spectra_wireless_testcases

--- run_spectra_wireless_testcases.py
+++ run_spectra_wireless_testcases.py
@@ -1,8 +1,13 @@
 #!/router/bin/python
+from __future__ import print_function
 import datetime, subprocess, os, sys
+import re
+import smtplib
+import mimetypes
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-def grep(path, regex):
-    import re
+def find_pattern_in_dir(path, regex):
     regObj = re.compile(regex)
     for root, dirs, fnames in os.walk(path):
         for fname in fnames:
@@ -10,18 +15,29 @@ def grep(path, regex):
                 if regObj.search(line):
                     return True
 
+def find_files_in_dir(path, regex):
+    regObj = re.compile(regex)
+    matched = []
+    for root, dirs, fnames in os.walk(path):
+        matched += [f for f in fnames if regObj.match(f)]
+    return matched
+
 class Workspace (object):
+    def __repr__ (self) :
+        return "<Workspace of branch %s at BINOS_ROOT=%s>" % (self.branch, self.binos_root)
+
     def __init__(self, directory = '', branch = 'macallan_dev'):
         self.ws_dir = directory
         self.branch = branch
         self.binos_root = "%s/binos" % self.ws_dir if self.ws_dir != '' else ''
+        self.spectra_dir = self.binos_root + '/platforms/ngwc/doppler_sdk/spectra/'
 
     def pull (self):
         """Pull work space"""
         if self.branch == '': 
             raise ValueError("Branch is not set")
         if self.ws_dir == '':
-           raise ValueError("workspace dir is not set")
+            raise ValueError("workspace dir is not set")
 
         try:
             if not os.path.exists(self.ws_dir):
@@ -37,7 +53,7 @@ class Workspace (object):
                 raise
             
     def build (self, target = 'x86_64_binos_root'):
-        if grep('%s/BUILD_LOGS/' % self.binos_root,
+        if find_pattern_in_dir('%s/BUILD_LOGS/' % self.binos_root,
                 'SUCC.*x86_64_binos_root'):
             print('x86_64_binos_root already built')
             return
@@ -55,10 +71,12 @@ class Workspace (object):
         except Exception as ex:
             template = "An exception of type {0} occured. Arguments:\n{1!r}"
             message = template.format(type(ex).__name__, ex.args)
-            print message
+            print(message)
 
-    def __repr__ (self) :
-        return "<Workspace of branch %s at BINOS_ROOT=%s>" % (self.branch, self.binos_root)
+    def get_wireless_testcases(self):
+        return [w.replace('.py', '') for w in 
+            find_files_in_dir(self.spectra_dir + '/scripts/test_suite/Wireless/',
+                                 '.*.py')]
 
 class Spectra(object):
     ws = None
@@ -77,7 +95,7 @@ class Spectra(object):
                     "-a", self.asic, "-b", binos_root]
         try:
             print("Executing (%s)" % ' '.join(buildCmd))
-            subprocess.check_call(buildCmd, shell=True)
+            subprocess.check_call(buildCmd)
         except subprocess.CalledProcessError as e:
             print("#### Spectra build failed", e.returncode)
    
@@ -89,16 +107,17 @@ class Spectra(object):
         buildCmd = [binos_root + "/platforms/ngwc/doppler_sdk/tools/scripts/spectra_build.py", "-p",
                     "-a", self.asic, "-b", binos_root, '-co']
         try:
-            subprocess.check_call(buildCmd, shell=True)
+            subprocess.check_call(buildCmd)
         except subprocess.CalledProcessError as e:
             print("#### Spectra clean failed", e.returncode)
  
 class RegressionRunner(object):
     ws = None
+    test_runner_exe = "/ws/siche-sjc/macallan/test_runner.py"
     def __init__(self, spectras=None):
         self.spectras = spectras
 
-    def prepare(self):
+    def prepare_ws(self):
         if self.ws is None:
             raise Exception('workspace not set')
 
@@ -106,18 +125,129 @@ class RegressionRunner(object):
         self.ws.pull()
         self.ws.build()
 
+    def cleanup_results(self):
+        binos_root = self.ws.binos_root
+        scripts_dir = "{}/platforms/ngwc/doppler_sdk/spectra/scripts".format(binos_root)
+        # If there are any core files in the directory then remove it
+        # before running the test.
+        for filename in os.listdir(scripts_dir):
+            path = os.path.join(scripts_dir, filename)
+            if not os.path.isfile(path):
+                continue
+            if "core" in filename.lower():
+                os.remove(path)
+
+        results_dir = "{}/platforms/ngwc/doppler_sdk/spectra/results".format(binos_root)
+        # Remove the results directory and created it before each run
+        if os.path.exists(results_dir):
+            shutil.rmtree(results_dir)
+        os.makedirs(results_dir)
+
+    def run_test(self, asic = 'DopplerD', tests = []):
+        cmd = [self.test_runner_exe, '-p', '-a', asic,
+            '-t', ':'.join(tests[5:7]), '-r', '"TESTMODE=FEATURE"',
+            '-b', self.ws.binos_root]
+        print("\nExecuting(%s)" % ' '.join(cmd))
+        try:
+            output = ''
+            p = subprocess.Popen(cmd, stdout = subprocess.PIPE, bufsize=1, 
+                      universal_newlines=True)
+            with p.stdout:
+                for line in iter(p.stdout.readline, b''):
+                    print(line, end='')
+                    output += line
+            p.wait()
+        except subprocess.CalledProcessError as e:
+            print("#### test_runner error: %s" % e)
+     
+        return output
+
+    @staticmethod
+    def get_wireless_testcases_from_csv ():
+        implemented_testcases = []
+        with open("/ws/siche-sjc/macallan/Wireless-Test-Plan.2016.csv") as f:
+            for line in f:
+                words = line.split(',')
+                if words[3] != "MISSING":
+                    implemented_testcases.append(words[2])
+        return implemented_testcases
+
+    @staticmethod
+    def parse_test_runner_output (output):
+        """
+        Convert text output to test results in a dict
+        """
+        results = {}
+        if "Results" not in output:
+            return None
+        output = output.split('Results')[1]
+        for l in output.split('\n'):
+            if '|' in l:
+                results[l.split('|')[1].strip()] = l.split('|')[2].strip()
+    
+        return results
+
+class Reporter (object):
+    emails = []
+    def __init__(self, emails = []):
+        self.emails = emails
+
+    def send_email(self, subject, body):
+        '''
+        Construction an email message and send it to the email
+        address provided. This function will get the workspace
+        information and other information to construct the
+        email.
+        '''
+        smtp_inst = smtplib.SMTP('localhost')
+        for email in self.emails :
+            outer = MIMEMultipart()
+            outer['To'] = email
+            outer['From'] = os.environ['USER'] + "@cisco.com"
+            outer['Subject'] = subject 
+        
+            outer.attach(MIMEText(body, "plain"))
+            composed = outer.as_string()
+            smtp_inst.sendmail(email, email, composed)
+
+         smtp_inst.quit()
+    
+    def compose(self, results):
+        cs=sum(1 for res in output['CS'].values() if res == 'PASSED')
+        d=sum(1 for res in output['D'].values() if res == 'PASSED')
+        body += """
+        Summary:
+        DopplerCS: {cs}/{total}
+        DopplerD: {d}/{total}\n""".format(cs = cs, d = d, total = len(output['CS'].keys()))
+        testcases = output[output.keys()[0]].keys()
+        tbl_format = '| {:<35} | {:<25} | {:<25} |'
+        body += tbl_format.format('Testname', 'DopplerCS', 'DopplerD')+'\n'
+        body += '\n'.join([tbl_format.format(t, output['CS'][t], output['D'][t]) for t in testcases])
+    
+        body += """
+        All tests were run with new code, feature mode.
+        """
+
+
+
+
 def run():
     STORAGE = '/scratch/siche'
     WS_NAME = "mac_" + datetime.datetime.now().strftime('%Y-%m-%d')
     WS_NAME = 'gogogo'
     r = RegressionRunner();
-    w = Workspace(directory = "%s/%s" % (STORAGE, WS_NAME))
-    r.ws = w;
-    r.prepare()
-    spectras = [Spectra(asic = 'DopplerD', ws = w), 
-                Spectra(asic = 'DopplerCS', ws = w)]
-    map(lambda sp: sp.build(), spectras)
+    r.ws = Workspace(directory = "%s/%s" % (STORAGE, WS_NAME))
+    r.prepare_ws()
 
+    spectras = [Spectra(asic = 'DopplerD', ws = r.ws), 
+                Spectra(asic = 'DopplerCS', ws = r.ws)]
+#    map(lambda sp: sp.build(), spectras)
+    r.spectras = spectras
+
+    results = {asic:r.parse_test_runner_output(r.run_test(asic, r.ws.get_wireless_testcases()))
+        for asic in ['DopplerCS', 'DopplerD']}
+
+    r = Reporter(['siche@cisco.com'])
 
 if __name__ == "__main__":
     run()
